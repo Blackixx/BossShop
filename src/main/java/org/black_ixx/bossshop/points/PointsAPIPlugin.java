0c9ccb2a0b75a5a1cf2fdf567d2f09e6ff8823f3
Felix Neubauer <felix@neuby.de>
2016-02-20 17:20:33 +0100

    *[ADD] Added PointsAPI support *[IMPROVE] When TransactionLog is enabled: No longer logging when player switches the server of clicks a shop-link *[IMPROVE] Players can now sell items with unsafe enchantments

---
+++ src/main/java/org/black_ixx/bossshop/points/PointsAPIPlugin.java
@@ -0,0 +1,47 @@
+package org.black_ixx.bossshop.points;
+
+import org.black_ixx.bossshop.BossShop;
+import org.black_ixx.bossshop.managers.ClassManager;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.plugin.Plugin;
+
+import me.BukkitPVP.PointsAPI.PointsAPI;
+
+public class PointsAPIPlugin extends IPointsAPI {
+	private PointsAPI pp;
+	private BossShop plugin;
+
+	public PointsAPIPlugin() {
+		super("PointsAPI");
+
+		plugin = ClassManager.manager.getPlugin();
+		Plugin pointsApi = plugin.getServer().getPluginManager().getPlugin("PointsAPI");
+		if (pointsApi != null) {
+			pp = ((PointsAPI) pointsApi);
+		}
+	}
+
+	@Override
+	public int getPoints(OfflinePlayer player) {
+		return pp.getPoints(player);
+	}
+
+	@Override
+	public int setPoints(OfflinePlayer player, int points) {
+		pp.setPoints(player.getPlayer(), points);
+		return getPoints(player);
+	}
+
+	@Override
+	public int takePoints(OfflinePlayer player, int points) {
+		pp.removePoints(player, points);
+		return getPoints(player);
+	}
+
+	@Override
+	public int givePoints(OfflinePlayer player, int points) {
+		pp.addPoints(player, points);
+		return getPoints(player);
+	}
+
+}
