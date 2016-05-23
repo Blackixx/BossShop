524f3473807f3c530186c2fa5ee5de0af05ce117
Felix Neubauer <fe33@neuby.de>
2016-05-23 14:43:35 +0200

    Fixed serverpinging

--- src/main/java/org/black_ixx/bossshop/managers/serverpinging/Connector3.java
+++ src/main/java/org/black_ixx/bossshop/managers/serverpinging/Connector3.java
@@ -24,17 +24,17 @@ public class Connector3 extends BasicConnector{
 	private Gson gson = new Gson();
 
 	private StatusResponse response;
-	
-	private long latest_failure;
-	
 
-	
-	
+	private long latest_failure;
+
+
+
+
 	public Connector3(String host, int port, int timeout){
 		setAddress(new InetSocketAddress(host, port));
 		setTimeout(timeout);		
 	}
-		
+
 
 	public InetSocketAddress getAddress() {
 		return this.host;
@@ -46,12 +46,12 @@ public class Connector3 extends BasicConnector{
 	public String getHost() {
 		return host.getAddress().getHostName();
 	}
-	
+
 	@Override
 	public int getPort(){
 		return host.getPort();
 	}	
-	
+
 	@Override
 	public String getPlayerCount() {
 		if(response==null){
@@ -68,16 +68,16 @@ public class Connector3 extends BasicConnector{
 		return response.getDescription();
 	}
 
-	
-	
+
+
 	public void setAddress(InetSocketAddress host) {
 		this.host = host;
 	}
 	void setTimeout(int timeout) {
 		this.timeout = timeout;
 	}
-	
-	
+
+
 	@Override
 	public void update(){
 		try {
@@ -89,11 +89,11 @@ public class Connector3 extends BasicConnector{
 			}
 		}
 	}
-	
-	
-	
 
-	
+
+
+
+
 	public int readVarInt(DataInputStream in) throws IOException {
 		int i = 0;
 		int j = 0;
@@ -116,96 +116,96 @@ public class Connector3 extends BasicConnector{
 			paramInt >>>= 7;
 		}
 	}
-	
-	
-    @SuppressWarnings("resource")
+
+
+	@SuppressWarnings("resource")
 	public StatusResponse fetchData() throws IOException {
 
-        Socket socket = new Socket();
-        OutputStream outputStream;
-        DataOutputStream dataOutputStream;
-        InputStream inputStream;
-        InputStreamReader inputStreamReader;
+		Socket socket = new Socket();
+		OutputStream outputStream;
+		DataOutputStream dataOutputStream;
+		InputStream inputStream;
+		InputStreamReader inputStreamReader;
 
-        socket.setSoTimeout(this.timeout);
+		socket.setSoTimeout(this.timeout);
 
-        socket.connect(host, timeout);
+		socket.connect(host, timeout);
 
-        outputStream = socket.getOutputStream();
-        dataOutputStream = new DataOutputStream(outputStream);
+		outputStream = socket.getOutputStream();
+		dataOutputStream = new DataOutputStream(outputStream);
 
-        inputStream = socket.getInputStream();
-        inputStreamReader = new InputStreamReader(inputStream);
+		inputStream = socket.getInputStream();
+		inputStreamReader = new InputStreamReader(inputStream);
 
-        ByteArrayOutputStream b = new ByteArrayOutputStream();
-        DataOutputStream handshake = new DataOutputStream(b);
-        handshake.writeByte(0x00); //packet id for handshake
-        writeVarInt(handshake, 4); //protocol version
-        writeVarInt(handshake, this.host.getHostString().length()); //host length
-        handshake.writeBytes(this.host.getHostString()); //host string
-        handshake.writeShort(host.getPort()); //port
-        writeVarInt(handshake, 1); //state (1 for handshake)
+		ByteArrayOutputStream b = new ByteArrayOutputStream();
+		DataOutputStream handshake = new DataOutputStream(b);
+		handshake.writeByte(0x00); //packet id for handshake
+		writeVarInt(handshake, 4); //protocol version
+		writeVarInt(handshake, this.host.getHostString().length()); //host length
+		handshake.writeBytes(this.host.getHostString()); //host string
+		handshake.writeShort(host.getPort()); //port
+		writeVarInt(handshake, 1); //state (1 for handshake)
 
-        writeVarInt(dataOutputStream, b.size()); //prepend size
-        dataOutputStream.write(b.toByteArray()); //write handshake packet
+		writeVarInt(dataOutputStream, b.size()); //prepend size
+		dataOutputStream.write(b.toByteArray()); //write handshake packet
 
 
-        dataOutputStream.writeByte(0x01); //size is only 1
-        dataOutputStream.writeByte(0x00); //packet id for ping
-        DataInputStream dataInputStream = new DataInputStream(inputStream);
-       // int size = readVarInt(dataInputStream); //size of packet
-        int id = readVarInt(dataInputStream); //packet id
-        
-        if (id == -1) {
-            throw new IOException("Premature end of stream.");
-        }
-        
-        if (id != 0x00) { //we want a status response
-            throw new IOException("Invalid packetID");
-        }
-        int length = readVarInt(dataInputStream); //length of json string
-        
-        if (length == -1) {
-            throw new IOException("Premature end of stream.");
-        }
+		dataOutputStream.writeByte(0x01); //size is only 1
+		dataOutputStream.writeByte(0x00); //packet id for ping
+		DataInputStream dataInputStream = new DataInputStream(inputStream);
+		int size = readVarInt(dataInputStream); //size of packet
+		int id = readVarInt(dataInputStream); //packet id
 
-        if (length == 0) {
-            throw new IOException("Invalid string length.");
-        }
-        
-        byte[] in = new byte[length];
-        dataInputStream.readFully(in);  //read json string
-        String json = new String(in);
-        
-        
-        long now = System.currentTimeMillis();
-        dataOutputStream.writeByte(0x09); //size of packet
-        dataOutputStream.writeByte(0x01); //0x01 for ping
-        dataOutputStream.writeLong(now); //time!?
+		if (id == -1) {
+			throw new IOException("Premature end of stream.");
+		}
 
-        readVarInt(dataInputStream);
-        id = readVarInt(dataInputStream);
-        if (id == -1) {
-            throw new IOException("Premature end of stream.");
-        }
-        
-        if (id != 0x01) {
-            throw new IOException("Invalid packetID");
-        }
-        long pingtime = dataInputStream.readLong(); //read response
+		if (id != 0x00) { //we want a status response
+			throw new IOException("Invalid packetID");
+		}
+		int length = readVarInt(dataInputStream); //length of json string
 
-        StatusResponse response = gson.fromJson(json, StatusResponse.class);
-        response.setTime((int) (now - pingtime));
-        
-        dataOutputStream.close();
-        outputStream.close();
-        inputStreamReader.close();
-        inputStream.close();
-        socket.close();
-        
-        return response;
-    }
-    	
+		if (length == -1) {
+			throw new IOException("Premature end of stream.");
+		}
+
+		if (length == 0) {
+			throw new IOException("Invalid string length.");
+		}
+
+		byte[] in = new byte[length];
+		dataInputStream.readFully(in);  //read json string
+		String json = new String(in);
+
+
+		long now = System.currentTimeMillis();
+		dataOutputStream.writeByte(0x09); //size of packet
+		dataOutputStream.writeByte(0x01); //0x01 for ping
+		dataOutputStream.writeLong(now); //time!?
+
+		readVarInt(dataInputStream);
+		id = readVarInt(dataInputStream);
+		if (id == -1) {
+			throw new IOException("Premature end of stream.");
+		}
+
+		if (id != 0x01) {
+			throw new IOException("Invalid packetID");
+		}
+		long pingtime = dataInputStream.readLong(); //read response
+
+		StatusResponse response = gson.fromJson(json, StatusResponse.class);
+		response.setTime((int) (now - pingtime));
+
+		dataOutputStream.close();
+		outputStream.close();
+		inputStreamReader.close();
+		inputStream.close();
+		socket.close();
+
+		return response;
+	}
+
 	public class StatusResponse {
 		private String description;
 		private Players players;
@@ -237,7 +237,7 @@ public class Connector3 extends BasicConnector{
 			this.time = time;
 		}
 	}
-	
+
 	public class Players {
 		private int max;
 		private int online;
