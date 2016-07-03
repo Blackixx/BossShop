c86fe41c8cebf6d49204996d2e8e96e07aac163e
Felix Neubauer <fe33@neuby.de>
2016-07-03 11:55:07 +0200

    removed "no points plugin" message

--- src/main/java/org/black_ixx/bossshop/points/FailedPointsAPI.java
+++ src/main/java/org/black_ixx/bossshop/points/FailedPointsAPI.java
@@ -1,6 +1,5 @@
 package org.black_ixx.bossshop.points;
 
-import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 
 public class FailedPointsAPI extends IPointsAPI {
@@ -11,31 +10,32 @@ public class FailedPointsAPI extends IPointsAPI {
 
 	public int getPoints(OfflinePlayer player) {
 		informPlayer(player);
-		return 0;
+		return -1;
 	}
 
 	public int setPoints(OfflinePlayer player, int points) {
 		informPlayer(player);
-		return 0;
+		return -1;
 	}
 
 	public int takePoints(OfflinePlayer player, int points) {
 		informPlayer(player);
-		return 0;
+		return -1;
 	}
 
 	public int givePoints(OfflinePlayer player, int points) {
 		informPlayer(player);
-		return 0;
+		return -1;
 	}
 
-	private void informPlayer(OfflinePlayer player) {
-		Bukkit.getConsoleSender().sendMessage("PlayerPoints/CommandPoints was not found... " + "You need one of that plugins if you want to work with Points! " + "Get PlayerPoints there: " + "http://dev.bukkit.org/server-mods/playerpoints/");
+	private void informPlayer(OfflinePlayer player) { //Not sending to prevent spam on servers that installed BossShop for the first time. It should be enough when BossShop notifies about this on startup. 
+		/*Bukkit.getConsoleSender().sendMessage("PlayerPoints/CommandPoints was not found... " + "You need one of that plugins if you want to work with Points! " + "Get PlayerPoints there: " + "http://dev.bukkit.org/server-mods/playerpoints/");
 		if(player.isOnline())
 			if(player.isOp()){
 				player.getPlayer().sendMessage("[BossShop] No Points Plugin installed. If you want to work with Points please install one.");
 			}else{
 				player.getPlayer().sendMessage("[BossShop] No Points Plugin installed. Please inform an administrator.");
 			}
+		 */
 	}
 }
