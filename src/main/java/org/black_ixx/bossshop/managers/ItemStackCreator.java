1f0113d6c5db1f6cd4c160b2a56d74f2d01fc325
Aron Sisask <aron.sisask@gmail.com>
2016-07-09 22:40:31 +0300

    Item can now have "headtexture" property which specifies texture ID rather than player name

--- src/main/java/org/black_ixx/bossshop/managers/ItemStackCreator.java
+++ src/main/java/org/black_ixx/bossshop/managers/ItemStackCreator.java
@@ -1,9 +1,14 @@
 package org.black_ixx.bossshop.managers;
 
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
+import java.util.UUID;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import org.black_ixx.bossshop.managers.external.GuiShopManagerManager;
 import org.bukkit.Bukkit;
 
@@ -377,6 +382,35 @@ public class ItemStackCreator {
 				i=gi; 
 				continue;
 			}
+
+			if (s.equalsIgnoreCase("headtexture")) {
+				if (i.getType() == Material.SKULL_ITEM) {
+					SkullMeta itemMeta = (SkullMeta) i.getItemMeta();
+
+					String url = "http://textures.minecraft.net/texture/" + a;
+
+					GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+					byte[] encodedData = Base64.getEncoder().encode(String.format("{textures:{SKIN:{url:\"%s\"}}}", url).getBytes());
+					profile.getProperties().put("textures", new Property("textures", new String(encodedData)));
+					Field profileField = null;
+					try {
+						profileField = itemMeta.getClass().getDeclaredField("profile");
+					} catch (NoSuchFieldException | SecurityException e) {
+						e.printStackTrace();
+					}
+					assert profileField != null;
+					profileField.setAccessible(true);
+					try {
+						profileField.set(itemMeta, profile);
+					} catch (IllegalArgumentException | IllegalAccessException e) {
+						e.printStackTrace();
+					}
+					i.setItemMeta(itemMeta);
+				} else {
+					ClassManager.manager.getBugFinder().severe("Mistake in Config: cannot set head texture for non skull item");
+				}
+			}
+
 
 
 		}
