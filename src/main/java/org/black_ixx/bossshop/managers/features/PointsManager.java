9cb12fec68546f634595c73c3249b75823b34981
Felix Neubauer <fe33@neuby.de>
2016-08-21 23:05:09 +0200

    improved PointsManager

--- src/main/java/org/black_ixx/bossshop/managers/features/PointsManager.java
+++ src/main/java/org/black_ixx/bossshop/managers/features/PointsManager.java
@@ -49,29 +49,28 @@ public class PointsManager {
 	}
 
 	public PointsManager() {
-		PointsPlugin p = ClassManager.manager.getSettings().getPointsPlugin();
+		this(ClassManager.manager.getSettings().getPointsPlugin());
+	}
+	
+	public PointsManager(PointsPlugin p) {
 		if (p == null) {
 			pa = new FailedPointsAPI();
 			return;
 		}
 
+		if(Bukkit.getPluginManager().getPlugin(p.getPluginName()) == null){
+			ClassManager.manager.getBugFinder().severe("You defined "+p.getPluginName()+" as Points Plugin... BUT IT WAS NOT FOUND?! Please install it or use an alternative like PlayerPoints (http://dev.bukkit.org/server-mods/playerpoints/). If you want BossShop to auto-detect your Points plugin simply set 'PointsPlugin: auto-detect'.");
+			pa = new FailedPointsAPI();
+			return;
+		}
 
 		switch (p) {
 
 		case COMMANDPOINTS:
-			if (Bukkit.getPluginManager().getPlugin("CommandPoints") == null) {
-				ClassManager.manager.getBugFinder().severe("You defined CommandPoints as the Points Plugin... BUT IT WAS NOT FOUND?! Please download it at Bukkit.org!");
-				break;
-			}
 			pa = new CommandPointsAPI();
 			break;
 
 		case ENJIN_MINECRAFT_PLUGIN:
-			if (Bukkit.getPluginManager().getPlugin("EnjinMinecraftPlugin") == null) {
-				ClassManager.manager.getBugFinder().severe("You defined Enjin Minecraft Plugin as the Points Plugin... BUT IT WAS NOT FOUND?! Please download it at Bukkit.org!");
-				break;
-			}
-
 			Plugin enjinplugin = Bukkit.getPluginManager().getPlugin("EnjinMinecraftPlugin");
 			if(enjinplugin.getDescription().getVersion().startsWith("2")){ //When using an older Enjin version
 				pa = new EnjinPointsAPI_v2();
@@ -82,60 +81,40 @@ public class PointsManager {
 			break;
 
 		case PLAYERPOINTS:
-			if (Bukkit.getPluginManager().getPlugin("PlayerPoints") == null) {
-				ClassManager.manager.getBugFinder().severe("You defined PlayerPoints as the Points Plugin... BUT IT WAS NOT FOUND?! Please download it at Bukkit.org!");
-				break;
-			}
 			pa = new PlayerPointsAPI();
 			break;
 
 		case POINTSAPI:
-			if (Bukkit.getPluginManager().getPlugin("PointsAPI") == null) {
-				ClassManager.manager.getBugFinder().severe("You defined PointsAPI as the Points Plugin... BUT IT WAS NOT FOUND?! Please download it at Bukkit.org!");
-				break;
-			}
 			pa = new PointsAPIPlugin();
 			break;
 
 		case TOKENENCHANT:
-			if (Bukkit.getPluginManager().getPlugin("TokenEnchant") == null) {
-				ClassManager.manager.getBugFinder().severe("You defined TokenEnchant as the Points(/Token) Plugin ... BUT IT WAS NOT FOUND?! Please download it at spigotmc.org!");
-				break;
-			}
 			pa = new TokenEnchantAPIPlugin();
 			break;
 
-		case VOTINGPLUGIN:
-			if (Bukkit.getPluginManager().getPlugin("VotingPlugin") == null) {
-				ClassManager.manager.getBugFinder().severe("You defined VotingPlugin as the Points Plugin ... BUT IT WAS NOT FOUND?! Please download it at spigotmc.org!");
-				break;
-			}
-			pa = new VotingPluginAPI();
-			break;
-
 		case Jobs:
-			if (Bukkit.getPluginManager().getPlugin("Jobs") == null) {
-				ClassManager.manager.getBugFinder().severe("You defined Jobs as the Point Plugin ... BUT IT WAS NOT FOUND?! Please download it at spigotmc.org!");
-				break;
-			}
 			pa = new JobsPointsAPI();
+			break;
+			
+		case VOTINGPLUGIN:
+			pa = new VotingPluginAPI();
 			break;
 
 		case CUSTOM:
 			IPointsAPI customPoints = PointsAPI.get(p.getCustom());
-			if (customPoints != null) {
-				pa = customPoints;
+			if (customPoints == null) {
 				break;
 			}	
+			pa = customPoints;
 			break;
 
 		}
+
 
 		if(pa == null){
 			ClassManager.manager.getBugFinder().warn("No PointsPlugin was found... You need one if you want BossShop to work with Points! Get PlayerPoints here: http://dev.bukkit.org/server-mods/playerpoints/");		
 			pa = new FailedPointsAPI();
 		}
-
 
 	}
 
